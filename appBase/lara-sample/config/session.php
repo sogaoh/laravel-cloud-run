7d5977ec4b15581df30e230538b6f3b53c0c8df4
sogaoh <sogaoh@ant-in-giant.work>
2021-11-07 10:58:03 +0900

    use Redis

--- appBase/lara-sample/config/session.php
+++ appBase/lara-sample/config/session.php
@@ -168,7 +168,10 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env(
+        'SESSION_SECURE_COOKIE',
+        env('APP_ENV') === 'local' ? false : true
+    ),
 
     /*
     |--------------------------------------------------------------------------
